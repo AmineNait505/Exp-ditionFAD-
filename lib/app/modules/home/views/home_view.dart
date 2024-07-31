a0b2d14479789cd52b4d00ef6d11f9b7ce5dbb12
Nait Slimene Amine <137489572+AmineNait505@users.noreply.github.com>
2024-07-31 11:59:47 +0100

    second commit

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -8,52 +8,53 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white, // Set the background color to white
       body: Center(
         child: SingleChildScrollView(
-
-          child:Column(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'images/fad.png',
-              width: 200,
-              height: 100,
-            ),
-            const SizedBox(height: 50),
-            Container(
-              width: 300, // Adjust the width based on your layout
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius: BorderRadius.circular(8),
+            children: [
+              Image.asset(
+                'assets/images/fad.png',
+                width: 200,
+                height: 100,
               ),
-              child: Obx(
-                    () => DropdownButtonHideUnderline(
-                  child: DropdownButton<String>(
-                    isExpanded: true,
-                    value: controller.selectedOption.value.isEmpty
-                        ? null
-                        : controller.selectedOption.value,
-                    icon: const Icon(Icons.arrow_drop_down),
-                    onChanged: (String? newValue) {
-                      if (newValue != null) {
-                        controller.setSelectedOption(newValue);
-                      }
-                    },
-                    items: <String>['Option 1', 'Option 2', 'Option 3']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
+              const SizedBox(height: 50),
+              Container(
+                width: 300, // Adjust the width based on your layout
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Obx(
+                      () => DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                      isExpanded: true,
+                      value: controller.selectedOption.value.isEmpty
+                          ? null
+                          : controller.selectedOption.value,
+                      icon: const Icon(Icons.arrow_drop_down),
+                      onChanged: (String? newValue) {
+                        if (newValue != null) {
+                          controller.setSelectedOption(newValue);
+                        }
+                      },
+                      items: <String>['Option 1', 'Option 2', 'Option 3']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
-      )  );
+    );
   }
 }
